6ab7e47e09ad5c9f960382aed09c06226cb94a19
Sahana Tankala <sahanatankala@gmail.com>
2021-08-13 16:00:34 -0400

    Finish Value Parsing

--- src/Schemey/Parsing.hs
+++ src/Schemey/Parsing.hs
@@ -3,21 +3,23 @@
 
 module Schemey.Parsing
     ( readExpression
+    , SValue
     ) where
 
 import System.Environment ()
-import Text.ParserCombinators.Parsec ( oneOf, many, parse, Parser, skipMany1, (<|>), char, noneOf, many1, newline, tab, alphaNum, octDigit, try )
+import Text.ParserCombinators.Parsec ( oneOf, many, parse, Parser, skipMany1, (<|>), char, noneOf, many1, newline, tab, alphaNum, octDigit, try, sepBy, endBy, sepEndBy )
 import Text.Read (reset)
 import Data.Bool (bool)
 import Control.Monad (liftM)
 import Text.Parsec.Char
-    ( digit, space, letter, endOfLine, hexDigit, anyChar, string )
+    ( digit, space, letter, endOfLine, hexDigit, anyChar, string, spaces )
 import GHC.Show (Show)
 import Numeric (readHex, readOct, readFloat)
 import Data.Char (toTitle, digitToInt)
 import GHC.Real (Real, (%))
 import GHC.Unicode (toTitle)
 import Data.Bits (toIntegralSized)
+import GHC.Arr (Array, listArray)
 
 -- | Data Definition for a Scheme Value
 data SValue = SAtom String
@@ -30,6 +32,8 @@ data SValue = SAtom String
             | SFloat Float
             | SList [SValue]
             | SDottedList [SValue] SValue
+            | SVector (Array Int SValue)
+            | SNil ()
             deriving (Show, Eq)
 
 -- | Reads a String of Schemey input and returns an output
@@ -38,19 +42,9 @@ readExpression input = case parse parseSValue "lisp" input of
     Left  err -> "No match: " ++ show err
     Right val   -> show val
 
-parseSValue :: Parser SValue
-parseSValue = parseAtom
-            <|> parseString
-            <|> try parseCharacter
-            <|> try parseBool
-            <|> try parseComplex
-            <|> try parseFloat
-            <|> try parseRational
-            <|> try parseInteger
-
 -- | Parser to ignore whitespace
-spaces :: Parser ()
-spaces = skipMany1 space
+spaces' :: Parser ()
+spaces' = skipMany1 space
 
 -- | Parses to recognize identifiers
 symbol :: Parser Char
@@ -69,6 +63,16 @@ escapeChar = do
                     't' -> '\t'
                     _ -> c
 
+-- | Main Parser for SValues
+-- delegates to other parsers when they are complex i.e. the number parser
+parseSValue :: Parser SValue
+parseSValue = parseAtom
+            <|> parseString
+            <|> try parseBool
+            <|> try parseCharacter
+            <|> try parseNumber
+            <|> parseList
+
 -- | Parses an SBool or an SAtom
 -- Will parse 'true' and 'false' as boolean literals
 -- all other symbols will be parsed as atoms
@@ -77,6 +81,15 @@ parseAtom = do
     first <- letter <|> symbol
     rest <- many $ letter <|> digit <|> symbol
     return $ SAtom (first : rest)
+
+-- | Parses an SString
+parseString :: Parser SValue
+parseString = do
+    char '"'
+    -- non quote character <|> \" <|> \'
+    s <- many $ escapeChar <|> noneOf "\"\\"
+    char '"'
+    return $ SString s
 
 -- | Parses an SBool
 parseBool :: Parser SValue
@@ -99,14 +112,21 @@ parseCharacter = do
                     [a] -> a
                     _ -> '\0'
 
+-- | Parser to handle all types of number formats
+parseNumber :: Parser SValue
+parseNumber = try parseComplex
+            <|> try parseFloat
+            <|> try parseRational
+            <|> try parseInteger
+
 -- | Parses an SNumber
 -- So far only parses whole numbers
 parseInteger :: Parser SValue
-parseInteger = parseDecimalInteger 
-            <|> parseDecimalIntegerStrict 
-            <|> parseHexadecimalInteger 
-            <|> parseOctalInteger 
-            <|> parseBinaryInteger 
+parseInteger = parseDecimalInteger
+            <|> parseDecimalIntegerStrict
+            <|> parseHexadecimalInteger
+            <|> parseOctalInteger
+            <|> parseBinaryInteger
 
 -- | Parses decimal numbers into SNumbers
 parseDecimalInteger :: Parser SValue
@@ -151,15 +171,6 @@ parseBinaryInteger = do
     x <- many1 $ oneOf "10"
     return $ SInteger (bin2num x 0)
 
--- | Parses an SString
-parseString :: Parser SValue
-parseString = do
-    char '"'
-    -- non quote character <|> \" <|> \'
-    s <- many $ escapeChar <|> noneOf "\"\\"
-    char '"'
-    return $ SString s
-
 -- | Parses an SComplex number
 parseComplex :: Parser SValue
 parseComplex = do
@@ -183,10 +194,80 @@ parseFloat = do
 -- | Parses an SRational number
 parseRational :: Parser SValue
 parseRational = do
-    numerator <- many1 digit 
+    numerator <- many1 digit
     char '/'
-    denominator <- many1 digit 
+    denominator <- many1 digit
     return $ SRational $ read numerator % read denominator
 
+
+-- | Parses either an SList, or an SDottedList
+parseList :: Parser SValue
+parseList = parseVector
+            <|> parseQuotedList
+            <|> parseQuasiquoteList
+            <|> try parseUnquoteSplicingList
+            <|> parseBackquoteList
+            <|> parseAnySList
+
+-- Parses either an SList or an SDottedList
+parseAnySList :: Parser SValue
+parseAnySList = do
+    char '(' >> spaces
+    head <- sepEndBy parseSValue spaces'
+    tail <- (char '.' >> spaces >> parseSValue) <|> return (SNil ())
+    spaces >> char ')'
+    return $ case tail of
+        SNil () -> SList head 
+        _ -> SDottedList head tail
+
+
 -- | Parses an SList
+-- TODO: needs to be updated for all expressions, not just values
+parseStandardList :: Parser SValue
+parseStandardList = SList <$> sepBy parseSValue spaces'
+
 -- | Parses an SDottedList
+-- TODO: needs to be updated for all expressions, not just values
+parseDottedList :: Parser SValue
+parseDottedList = do
+    head <- endBy parseSValue spaces'
+    tail <- char '.' >> spaces' >> parseSValue
+    return $ SDottedList head tail
+
+-- | Parses a Quoted list into an SList
+parseQuotedList :: Parser SValue
+parseQuotedList = do
+    char '\''
+    x <- parseSValue
+    return $ SList [SAtom "quote", x]
+
+
+-- | Parses an SList constructed with a quasiquote
+parseQuasiquoteList :: Parser SValue
+parseQuasiquoteList = do
+    char '`'
+    x <- parseSValue
+    return $ SList [SAtom "quasiquote", x]
+
+-- | Parses an SList constructed with a backquote
+parseBackquoteList :: Parser SValue
+parseBackquoteList = do
+    char ','
+    x <- parseSValue
+    return $ SList [SAtom "backquote", x]
+
+-- | Parses an SList constructed using unquote splicing
+parseUnquoteSplicingList :: Parser SValue
+parseUnquoteSplicingList = do
+    string ",@"
+    x <- parseSValue
+    return $ SList [SAtom "unquote-splicing", x]
+
+-- | Parses an SVector
+-- lists of the form #(<contents>) are parsed as vectors
+parseVector :: Parser SValue
+parseVector = do
+    string "#("
+    xs <- sepBy parseSValue spaces'
+    char ')'
+    return $ SVector $ listArray (0, length xs - 1) xs
